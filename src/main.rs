e9568fad664dace8001b1d0a6ee2ae96c8f32c39
Arie Roos <arie@itecho.co.za>
2019-08-24 20:51:17 +0200

    Changed handling of forward slash in relative URL

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ use actix_web::{web, App, Responder, HttpServer, HttpRequest};
 use std::path::PathBuf;
 
 fn index() -> impl Responder {
-    find_file("/index.html")
+    find_file("index.html")
 }
 
 fn static_file(name: HttpRequest) -> impl Responder {
-    find_file(name.path())
+    find_file(&name.path()[1..])
 }
 
 fn find_file(path: &str) -> impl Responder {
-    let in_path = path.parse::<PathBuf>().unwrap();
     let mut final_path = "frontend".parse::<PathBuf>().unwrap();
-    final_path.push(in_path.strip_prefix("/").unwrap());
+    final_path.push(path.parse::<PathBuf>().unwrap());
 
     match NamedFile::open(&final_path) {
         Ok(x) => Ok(x),
